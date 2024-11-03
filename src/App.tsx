c15ae553373d46c4912012edfc58bdee8a0aa511
David Prasad <d7prasad89@gmail.com>
2024-11-02 21:23:36 -0400

    Small changes in index page and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -11,11 +9,11 @@ function App() {
       </div>
       <h1>Welcome to Radio Heaven</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          Explore
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Listen to your favourite music and sermons
         </p>
       </div>
     </>
