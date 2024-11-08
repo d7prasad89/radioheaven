a32c26b3608a77f481441aa117b7ec153d0f34f6
David Prasad <d7prasad89@gmail.com>
2024-11-07 20:23:13 -0500

    Added tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,27 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div>
-          Whoever has ears, let them hear. - Matthew 11:15
-      </div>
-      <h1>Welcome to Radio Heaven</h1>
-      <div className="card">
-        <button>
-          Explore
-        </button>
-        <p>
-          Listen to your favourite music and sermons and more
-        </p>
-          <div className="card">
-              <button>Songs</button> &nbsp;
-              <button>Sermons</button>
+      <>
+          <div>
+              Whoever has ears, let them hear. - Matthew 11:15
           </div>
-      </div>
-    </>
+          <h1 className="text-5xl font-bold">
+              Welcome to Radio Heaven !!
+          </h1>
+          <div className="card">
+              <button>
+                  Explore
+              </button>
+              <p>
+                  Listen to your favourite music and sermons and more
+              </p>
+              <div className="card">
+                  <button>Songs</button>
+                  &nbsp;
+                  <button>Sermons</button>
+              </div>
+          </div>
+      </>
   )
 }
 
